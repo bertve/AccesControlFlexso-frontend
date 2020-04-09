745f8bcf220f3afc358566909731e51a7ddf2258
bert van eeckhoutte <bert.vaneeckhoutte@student.hogent.be>
2020-04-09 15:37:42 +0200

    changed name wallet item to office item

--- Flexsame/app/src/main/java/com/flexso/flexsame/ui/wallet/WalletItemListener.kt
+++ Flexsame/app/src/main/java/com/flexso/flexsame/ui/wallet/WalletItemListener.kt
@@ -3,9 +3,7 @@ package com.flexso.flexsame.ui.wallet
 import android.view.View
 import com.airbnb.lottie.LottieAnimationView
 import com.flexso.flexsame.R
-import com.flexso.flexsame.databinding.WalletItemBinding
 import com.flexso.flexsame.models.Office
-import kotlinx.android.synthetic.main.wallet_item.view.*
 
 class WalletItemListener(val clickListener: (itemId : Long) -> Unit) {
     fun onClick(v : View , o : Office) {
